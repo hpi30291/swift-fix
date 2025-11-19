ff59ed6e7cd7d3ee5012a8257931c40d5002f5d0
faisalarip <faisalarip10@gmail.com>
2025-11-19 21:37:56 +0700

    Initial Commit

---
+++ CADMVPermitPrep/CADMVPermitPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  CADMVPermitPrepApp.swift
+//  CADMVPermitPrep
+//
+//  Created by faisal nur arif on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CADMVPermitPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
